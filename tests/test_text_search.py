510682f0f01cbe214f71998144b94878a6b3fc97
Marius <marius.mather@sydney.edu.au>
2021-12-13 14:07:15 +1100

    Basic test of regex searching

--- tests/test_text_search.py
+++ tests/test_text_search.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import spacy
 
 from ant_widgets.text_search import prepare_text_df
@@ -11,6 +12,22 @@ def test_prepare_text_df(sherlock_holmes_dummy_df):
 
 def test_text_search(sherlock_holmes_dummy_df):
     df = prepare_text_df(sherlock_holmes_dummy_df)
+    # Matches "she" and "Sherlock"
     table = SearchTable(df, keyword="she")
     results = table.to_dataframe()
     assert (results["text_id"] == [0, 0, 2]).all()
+
+
+def test_text_search_regex():
+    df = pd.DataFrame(
+        {
+            "text_id": [1, 2, 3, 4],
+            "text": ["AGGACTTA", "invalid AGGACTA", "XPOAGTC", "TACCA"],
+        }
+    )
+    df = prepare_text_df(df, id_column="text_id")
+    # Basic DNA base matcher
+    # Only match if the entire string is valid DNA bases (A, C, G, T)
+    table = SearchTable(df, keyword=r"^[ACGT]+$", regex=True)
+    results = table.to_dataframe()
+    assert (results["text_id"] == [1, 4]).all()
