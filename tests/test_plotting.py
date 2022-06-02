cdb5c6242356cf075be4493c6a8de497afa7ec8c
Marius <marius.mather@sydney.edu.au>
2022-06-02 14:09:52 +1000

    Allow >256 plot colours

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -5,7 +5,8 @@ import pytest
 from atap_widgets.plotting import ConversationPlot
 
 
-@pytest.mark.parametrize("n", [2, 3, 10, 15, 20, 100])
+# bokeh palettes only have 256 colours so make sure we can generate more
+@pytest.mark.parametrize("n", [2, 3, 10, 15, 20, 100, 240, 256, 260])
 def test_get_palettes(n):
     hex_pattern = re.compile("#[a-f0-9]{3,6}")
     palette = ConversationPlot._get_categorical_palette(n)
